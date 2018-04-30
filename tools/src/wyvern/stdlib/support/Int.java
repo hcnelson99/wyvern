36cfa70bf26a8c0c25f3a8232ab3d4a6b3d9f70d
Andre Westerlund <andrewest_04@hotmail.com>
2018-04-30 16:58:42 +1200

    added compare

--- tools/src/wyvern/stdlib/support/Int.java
+++ tools/src/wyvern/stdlib/support/Int.java
@@ -6,4 +6,12 @@ public class Int {
     public int from(String s) {
         return Integer.parseInt(s.trim());
     }
+	
+	public int max_value(){
+		return Integer.MAX_VALUE;
+	}
+	
+	public int min_value(){
+		return Integer.MIN_VALUE;
+	}
 }
